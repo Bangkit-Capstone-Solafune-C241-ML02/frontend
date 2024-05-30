17627cf6d9ba61b51f97652d368dd4810b7231ad
AbiyaMakruf <aabbiiyyaa@gmail.com>
2024-05-30 14:11:57 +0700

    Mengubah default value untuk dropdown

--- js/index.js
+++ js/index.js
@@ -156,6 +156,8 @@ function createInputFields() {
     'B12 (SWIR 2)',
   ];
 
+  const defaultValues = ['B1 (Aerosols)', 'B2 (Blue)', 'B3 (Green)'];
+
   for (let i = 1; i <= 3; i++) {
     const select = document.createElement('select');
     select.classList.add('form-select', 'my-2');
@@ -165,74 +167,56 @@ function createInputFields() {
       const option = document.createElement('option');
       option.value = optionText;
       option.textContent = optionText;
+      if (optionText === defaultValues[i - 1]) {
+        option.selected = true;
+      }
       select.appendChild(option);
     });
 
     container.appendChild(select);
   }
 
-// Create Convert Button
-const convertButtonContainer = document.getElementById('convertButtonContainer');
-convertButtonContainer.innerHTML = ''; // Clear previous button if any
+  // Create Convert Button
+  const convertButtonContainer = document.getElementById('convertButtonContainer');
+  convertButtonContainer.innerHTML = ''; // Clear previous button if any
 
-const convertBtn = document.createElement('button');
-convertBtn.id = 'convertBtn';
-convertBtn.classList.add('btn', 'btn-primary', 'my-2');
-convertBtn.innerHTML = '<span id="convertText">Convert</span><span id="convertSpinner" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display: none;"></span> <i class="bi bi-arrow-repeat" id="convertIcon"></i>';
+  const convertBtn = document.createElement('button');
+  convertBtn.id = 'convertBtn';
+  convertBtn.classList.add('btn', 'btn-primary', 'my-2');
+  convertBtn.innerHTML = '<span id="convertText">Convert</span><span id="convertSpinner" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display: none;"></span> <i class="bi bi-arrow-repeat" id="convertIcon"></i>';
 
-convertBtn.addEventListener('click', async () => {
-  const convertText = document.getElementById('convertText');
-  const convertSpinner = document.getElementById('convertSpinner');
-  const convertIcon =  document.getElementById('convertIcon');
+  convertBtn.addEventListener('click', async () => {
+    const convertText = document.getElementById('convertText');
+    const convertSpinner = document.getElementById('convertSpinner');
+    const convertIcon = document.getElementById('convertIcon');
 
-  convertBtn.disabled = true;
-  convertText.textContent = "Loading...";
-  convertIcon.style.display = "none";
-  convertSpinner.style.display = "inline-block"; 
+    convertBtn.disabled = true;
+    convertText.textContent = "Loading...";
+    convertIcon.style.display = "none";
+    convertSpinner.style.display = "inline-block";
 
-  const selectedValues = [];
-  for (let i = 1; i <= 3; i++) {
-    const dropdown = document.getElementById(`dropdownField${i}`);
-    selectedValues.push(dropdown.value);
-  }
+    const selectedValues = [];
+    for (let i = 1; i <= 3; i++) {
+      const dropdown = document.getElementById(`dropdownField${i}`);
+      selectedValues.push(dropdown.value);
+    }
 
-  await sendDropdownValues(mapDropdownValues(selectedValues));
+    await sendDropdownValues(mapDropdownValues(selectedValues));
 
-  // Reset button
-  convertText.textContent = "Convert";
-  convertSpinner.style.display = "none";
-  convertIcon.style.display = "inline-block";
-  convertBtn.disabled = false;
-});
+    // Reset button
+    convertText.textContent = "Convert";
+    convertSpinner.style.display = "none";
+    convertIcon.style.display = "inline-block";
+    convertBtn.disabled = false;
+  });
 
-convertButtonContainer.appendChild(convertBtn);
-
+  convertButtonContainer.appendChild(convertBtn);
 }
 
-function mapDropdownValues(values) {
-  const mapping = {
-    'B1 (Aerosols)': 1,
-    'B2 (Blue)': 2,
-    'B3 (Green)': 3,
-    'B4 (Red)': 4,
-    'B5 (Red Edge 1)': 5,
-    'B6 (Red Edge 2)': 6,
-    'B7 (Red Edge 3)': 7,
-    'B8 (NIR)': 8,
-    'B8A (Red Edge 4)': 9,
-    'B9 (Water vapor)': 10,
-    'B11 (SWIR 1)': 11,
-    'B12 (SWIR 2)': 12
-  };
-
-  return values.map(value => mapping[value]);
-}
-
-// Fungsi untuk mengirim nilai dropdown ke server
 async function sendDropdownValues(values) {
   const config = await loadConfig();
   const endpoint = config.ENDPOINTCONVERT;
-  
+
   try {
     const response = await fetch(endpoint, {
       method: 'POST',
@@ -255,6 +239,26 @@ async function sendDropdownValues(values) {
   } catch (error) {
     console.error('Error sending dropdown values:', error);
   }
+}
+
+
+function mapDropdownValues(values) {
+  const mapping = {
+    'B1 (Aerosols)': 1,
+    'B2 (Blue)': 2,
+    'B3 (Green)': 3,
+    'B4 (Red)': 4,
+    'B5 (Red Edge 1)': 5,
+    'B6 (Red Edge 2)': 6,
+    'B7 (Red Edge 3)': 7,
+    'B8 (NIR)': 8,
+    'B8A (Red Edge 4)': 9,
+    'B9 (Water vapor)': 10,
+    'B11 (SWIR 1)': 11,
+    'B12 (SWIR 2)': 12
+  };
+
+  return values.map(value => mapping[value]);
 }
 
 // Fungsi umum untuk mengubah posisi peta
